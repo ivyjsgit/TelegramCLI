f085836ccc6207d13950da1f10f84aa0813a6551
Ivy J <34287279+ivyjsgit@users.noreply.github.com>
2019-01-18 21:47:31 -0600

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -1,4 +1,4 @@
-from telethon import TelegramClient, events, sync
+from telethon import TelegramClient, events, sync, types
 # import telethon.tl.types.PeerChannel
 from PyQt5.QtWidgets import QApplication, QLabel
 
@@ -17,53 +17,32 @@ def print_messages(numChats):
     entities = []
     for dialog in client.get_dialogs(limit = numChats):
         message = dialog.message
-       
+        entity = dialog.entity
+        # print(dialog.entity.stringify())
         if(message.from_id is None):
+            print("Message is from a channel!")
             peer_id = message.to_id.channel_id
             entity = message.to_id
             print('(' + str(count) + ') ' + dialog.name + ": " + message.message)
         else:
-            userID = message.from_id
-            try:
-                entity = message.from_id
-                peer_id = message.to_id.user_id
-                print("We got a single message!")
-                # print(message.stringify())
-                userName = getUsername(userID)
-                print('(' + str(count) + ') ' + dialog.name + ' - ' + userName + ": " + message.message)
-                print("Comparing peer_ID " + str(peer_id) + " " + str(ourID))
-                if(peer_id == ourID):   
-                    peer_id = message.to_id.user_id
-                    print("CHANGING ID TO " + peer_id)
-                    entity = message.to_id
-                else: 
-                    entity = message.to_id
-                # print(message)
-            except:
-                print("We got a group!")
-                # print(message.stringify())
-                entity = message.to_id
-                peer_id = message.to_id.channel_id
-                userName = getUsername(userID)
-                print('(' + str(count) + ') ' + dialog.name + ' - ' + userName + ": " + message.message)
-                if(peer_id == ourID):   
-                    peer_id = message.to_id.user_id
-                    print("CHANGING ID TO " + peer_id)
-                    entity = message.to_id
-        ids.append(str(peer_id))
+            from_id = message.from_id
+            message_sender_username = getUsername(from_id)
+            if(message.action is None):
+                print('(' + str(count) + ') ' + dialog.name + "- " + message_sender_username + ": "+ message.message)
+            elif(type(message.action) is types.MessageActionChatJoinedByLink):
+                print('(' + str(count) + ') ' + dialog.name + "- " + message_sender_username + " joined the group")
+
+        # ids.append(str(peer_id))
         entities.append(entity)
-        # print(message.stringify())
         count +=1
-    print(ids)
-    print(entities)
-    select_username(ids, entities)
+    select_username(entities)
     
-def select_username(ids, entities):
+def select_username(entities):
     userInput = input("Selection: ")
-    print("Loading selection: " + ids[int(userInput)])
     loadChat(entities[int(userInput)])
 
 def loadChat(entity):
+    print("Loading chat!!!")
     # messages = client.iter_messages(id)
     messages = client.get_messages(entity, limit = 10)
     messages.reverse()
@@ -71,18 +50,33 @@ def loadChat(entity):
         printTextualMessage(message)
     chatOptions(entity)
 def chatOptions(entity):
-    option = input("Options- (S)end : ")
-    if(option.lower == "s"):
+    option = input("Options- (S)end (R)efresh: ")
+    print(option)
+    if option.lower() == 's' :
+        print("Sending!!!")
         sendMessage(entity)
+    if option.lower() == 'r':
+        loadChat(entity)
 def sendMessage(entity):
     message = input("Message: ")
     client.send_message(entity, message=message)
+    chatOptions(entity)
 
 def printTextualMessage(message):
-    username = getUsername(message.from_id)
-    message = message.message
-    print(username + ": " + message)
-
+    # print(message)
+    try:
+        username = getUsername(message.from_id)
+        # print(message.action)
+        if(type(message.action) is types.MessageActionChatJoinedByLink):
+            print(username + " joined the group")
+        else:
+            message = message.message
+            print(username + ": " + message)
+    except:
+        if(message.action is types.MessageActionChatJoinedByLink):
+            print("Woah!")
+        # print(message)
+        print("Uhh")
 
 def getUsername(userID):
     user = client.get_entity(userID)
